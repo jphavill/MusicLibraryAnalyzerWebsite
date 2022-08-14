bb7c45dc79d9928af2f63d8e7271e4eae14e3e02
jphavill <jphavill@gmail.com>
2022-08-14 15:11:11 -0300

    temporarily disabled showing entire history of songs, since rendering it is the slowdown (will be turned back on with pagination so all 10000 aren't rendered) also enabled linking the list of artists to the graphview

--- src/app/graphs-page/graph-view/graph-view.component.ts
+++ src/app/graphs-page/graph-view/graph-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SongStatsService } from 'app/song-stats-service';
-import { LibraryStats } from 'models/stat.model'
+import { LibraryStats, ArtistStats } from 'models/stat.model'
 
 @Component({
   selector: 'app-graph-view',
@@ -9,11 +9,19 @@ import { LibraryStats } from 'models/stat.model'
 })
 export class GraphViewComponent implements OnInit {
   libraryStats: LibraryStats[] = Array();
+  artistStats: Map<string, ArtistStats> = new Map<string, ArtistStats>();
+  artistList: string[] = Array()
 
   constructor(private songStatsService: SongStatsService) { }
 
   ngOnInit(): void {
     this.songStatsService.libraryStats.subscribe(response => this.libraryStats = response)
+    this.songStatsService.artistStats.subscribe(response => this.artistStats = this.updateArtistStats(response))
+  }
+
+  updateArtistStats(artistStats: Map<string, ArtistStats>): Map<string, ArtistStats>{
+    this.artistList = [ ...artistStats.keys()]
+    return artistStats
   }
 
 
