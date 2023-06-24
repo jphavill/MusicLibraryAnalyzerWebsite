aa2aac8a1e7482cefaef185b6d2b97315495eb6f
jphavill <jphavill@gmail.com>
2023-06-24 18:39:57 -0300

    added FAQ page

---
+++ src/app/faq-page/faq-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-page',
+  templateUrl: './faq-page.component.html',
+  styleUrls: ['./faq-page.component.sass']
+})
+export class FaqPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
